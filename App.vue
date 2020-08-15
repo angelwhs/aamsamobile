b8928b2c8e9f69252aef28c950fa3731f141c40d
HaoSu Wang <angelwhs@hotmail.com>
2020-08-15 17:42:17 +0800

    first
    Signed-off-by: HaoSu Wang <angelwhs@hotmail.com>

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch');
+			
+			//
+			this.$store.dispatch('loadTabbarList');
+		},
+		onShow: function() {
+			console.log('App Show')
+			
+			
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "uview-ui/index.scss";
+	/*每个页面公共css */
+</style>
---
+++ pages/alumniassociation/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		
+		
+		<u-tabbar :list="vuex_tabbar" :mid-button="false"></u-tabbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
